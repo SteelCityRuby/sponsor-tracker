bfef4954d968741e0770d84609f9dc95613d8027
Justin Geibel <jtgeibel@gmail.com>
2014-03-09 20:49:01 -0400

    Only allow unauthenticated access to the Welcome and Session controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :ensure_authenticated
+
+  def ensure_authenticated
+    return if browserid_authenticated?
+    return if params[:controller] == 'welcome' || params[:controller] == 'sessions'
+    redirect_to root_url
+  end
 end
