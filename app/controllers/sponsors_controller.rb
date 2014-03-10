4386722c2ad1c0a15b99886d5cd5ebb7d603869a
Justin Geibel <jtgeibel@gmail.com>
2014-03-09 20:35:16 -0400

    Remove state from permitted fields on Sponsors

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -69,6 +69,6 @@ class SponsorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sponsor_params
-      params.require(:sponsor).permit(:name, :state)
+      params.require(:sponsor).permit(:name)
     end
 end
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,2 +1,3 @@
 class Sponsor < ActiveRecord::Base
+  validates_presence_of :name
 end
