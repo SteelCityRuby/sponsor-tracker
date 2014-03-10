7c2b2e7efe2488776d8da289bb779e24c72c4a31
Justin Geibel <jtgeibel@gmail.com>
2014-03-09 22:36:45 -0400

    Add basic workflow state machine

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,4 +1,22 @@
 class Sponsor < ActiveRecord::Base
   validates_presence_of :name
   validates_length_of :name, within: 5..40
+
+  state_machine :initial => :prospect do
+    event :pledge do
+      transition :prospect => :pledged
+    end
+
+    event :receive_payment do
+      transition :pledged => :payment_processing
+    end
+
+    event :reconsile do
+      transition :payment_processing => :payment_cleared
+    end
+
+    event :upload_assets do
+      transition :payment_cleared => :assets_uploaded
+    end
+  end
 end
