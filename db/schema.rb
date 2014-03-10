8d39801b533997d6465e79e8e5c2be3f480edd36
Justin Geibel <jtgeibel@gmail.com>
2014-03-09 22:44:00 -0400

    `rails g model PaymentType name`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310005220) do
+ActiveRecord::Schema.define(version: 20140310024334) do
+
+  create_table "payment_types", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sponsors", force: true do |t|
     t.string   "name"
