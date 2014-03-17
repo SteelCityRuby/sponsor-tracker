98c3c15501ea99334c86b8c283316f614558859c
Justin Geibel <jtgeibel@gmail.com>
2014-03-17 16:47:10 -0400

    Tweak views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317194927) do
+ActiveRecord::Schema.define(version: 20140317195703) do
+
+  create_table "package_addon_types", force: true do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.integer  "total_spots"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "package_types", force: true do |t|
     t.string   "name"
