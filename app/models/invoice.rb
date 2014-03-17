1767243700c1cc3e93ae6dd4d80a89b43510c951
Justin Geibel <jtgeibel@gmail.com>
2014-03-17 17:01:13 -0400

    Add validations and cleanup views

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,15 @@
 class Invoice < ActiveRecord::Base
   belongs_to :sponsor
   belongs_to :package_type
+
+  validates_presence_of :sponsor, :package_type
+  validates_uniqueness_of :sponsor
+
+  def total
+    package_type.price
+  end
+
+  def addons_allowed?
+    package_type.addons_allowed?
+  end
 end
