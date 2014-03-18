ea4bef2dc60fa23c3740ef1f6fc7499985a1f29d
Justin Geibel <jtgeibel@gmail.com>
2014-03-17 23:48:07 -0400

    Fix validations on invoice line items

--- app/models/invoice_addon_line_item.rb
+++ app/models/invoice_addon_line_item.rb
@@ -2,9 +2,10 @@ class InvoiceAddonLineItem < ActiveRecord::Base
   belongs_to :invoice
   belongs_to :package_addon_type
 
-  validates_presence_of :invoice
-  validates_presence_of :package_addon_type
+  validates_presence_of :invoice, :package_addon_type, :quantity
+
   validates_uniqueness_of :package_addon_type, scope: :invoice
+  validates_numericality_of :quantity, greater_than: 0
 
   def price
     quantity * package_addon_type.price
