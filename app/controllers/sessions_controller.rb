5bdaccdf392ff91da044e36f341c6ab2d208152b
Justin Geibel <jtgeibel@gmail.com>
2014-03-24 13:28:59 -0400

    Provide a more descriptive error for a GET request on /login
    A GET request against /login was resulting in a routing error.  Javascript should be intercepting this link and generating a POST with an identity assertion.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
+  # GET /login
+  def index
+    flash[:error] = "Authentication could not proceed.  Please ensure that javascript is enabled."
+    redirect_to root_url
+  end
+
   # POST /login
   def create
     respond_to_browserid
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   post 'login' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
+  get 'login' => 'sessions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
