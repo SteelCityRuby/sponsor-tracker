97d20cbcebadfdf7e4c4a5f5d5ef432d635e9809
Justin Geibel <jtgeibel@gmail.com>
2014-04-10 01:01:17 -0400

    Fix display of postgresql version info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,10 +7,11 @@ class WelcomeController < ApplicationController
 
   def about
     adapter = Rails.configuration.database_configuration[Rails.env]['adapter']
+    url = Rails.configuration.database_configuration[Rails.env]['url']
     if adapter == 'sqlite3'
       @db_server_version = ActiveRecord::Base.connection.execute('select sqlite_version()')[0][0]
       @db_adapter = "SQLite3"
-    elsif adapter == 'postgresql'
+    elsif adapter == 'postgresql' || /^postgresql/ =~ url
       @db_server_version = ActiveRecord::Base.connection.execute('select version()').first['version']
       @db_adapter = "PostgreSQL"
     else
