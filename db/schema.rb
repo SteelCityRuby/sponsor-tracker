ef1e4bf15b8145ce26861453b1b56a75e41387d1
Justin Geibel <jtgeibel@gmail.com>
2014-04-27 18:59:43 -0400

    Rename file to match class name
    Rails apparently looks up the class name based on the source file's name.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407204017) do
+ActiveRecord::Schema.define(version: 20140427175201) do
 
   create_table "invoice_addon_line_items", force: true do |t|
     t.integer  "invoice_id"
